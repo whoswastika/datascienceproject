c040351e8aa310b3d0800382334d93bb66b077e9
Tappu <swastikad164@gmail.com>
2025-08-08 13:09:39 +0530

    Data Transformation Done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+
 
 # logger.info("welcome to our cusom hehehe")
 
@@ -24,3 +26,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Data Transformation Pipeline"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.initiate_data_transformation()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+         
